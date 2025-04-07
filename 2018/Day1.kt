fd83b4b279d19c96b796ffb1bea14c9258d28caf
Darren Kirby <bulliver@gmail.com>
2025-04-07 10:36:47 -0700

    Started 2018 puzzles in Kotlin,and updated README

---
+++ 2018/Day1.kt
@@ -0,0 +1,30 @@
+import java.io.File
+
+fun readInput(): List<Int> {
+    val lines = File("./src/day1.txt").readLines()
+    val numbers = lines.map { it.removePrefix("+").toInt() }
+    return numbers
+}
+
+fun findFirstDuplicateFrequency(changes: List<Int>): Int {
+    val seen = mutableSetOf(0)
+    var freq = 0
+
+    while (true) {
+        for (delta in changes) {
+            freq += delta
+            if (!seen.add(freq)) {
+                return freq // found it!
+            }
+        }
+    }
+}
+
+fun main() {
+    val nums = readInput()
+    // part 1
+    println(nums.sum())
+    // part 2
+    println(findFirstDuplicateFrequency(nums))
+}
+
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# code_advent
+# Advent of Code
 My solutions for the Advent of Code programming challenges
 
-2015: Julia
-2016: Bash
-2017: Ruby
-2024: Python
+* 2015: Julia
+* 2016: Bash
+* 2017: Ruby
+* 2018: Kotlin
+* 2024: Python
 

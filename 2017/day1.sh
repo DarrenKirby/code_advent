f72beebec1eb654e170e99b853af1f77826e58c6
Darren Kirby <bulliver@gmail.com>
2025-04-23 16:14:38 -0700

    Clean up script (using shellcheck)

--- 2017/day1.sh
+++ 2017/day1.sh
@@ -3,27 +3,27 @@
 # Part 1
 FILE=./input/input.txt
 {
-read input
+read -r input
 } < $FILE
 
 score=0
 
 lastchar=""
 for (( i=0; i<${#input}; i++ )); do
-    if [[ $lastchar ==  ${input:i:1} ]]; then
-        score=$(( score + $lastchar ))
+    if [[ $lastchar ==  "${input:i:1}" ]]; then
+        score=$(( score + lastchar ))
     fi
     lastchar=${input:i:1}
 done
 
 # The array is 'circular', so last and first num go together
-score=$(( score + $lastchar ))
+score=$(( score + lastchar ))
 echo $score
 
 # Part 2
 FILE=./input/input.txt
 {
-read input
+read -r input
 } < $FILE
 
 score=0
@@ -33,18 +33,18 @@ offset=$(( ${#input} / 2 ))
 
 for (( i=0; i<${#input}; i++ )); do
     offset_i=$(( i + offset ))
-    if [[ $offset_i > ${#input} ]]; then
+    if [[ $offset_i -gt ${#input} ]]; then
         offset_i=$(( offset_i - ${#input} ))
     fi
-    if [[ ${input:i:1} ==  ${input:offset_i:1} ]]; then
+    if [[ ${input:i:1} ==  "${input:offset_i:1}" ]]; then
         score=$(( score + ${input:i:1} ))
     fi
     lastchar=${input:i:1}
 done
 
 # The array is circular, so last number goes with 0 + offset
-if [[ $lastchar == ${input:offset:1} ]]; then
-    score=$(( score + $lastchar ))
+if [[ $lastchar == "${input:offset:1}" ]]; then
+    score=$(( score + lastchar ))
 fi
 
 echo $score

91f67fcbb781f3b312b1ff4ab62a51d994e0d94e
Darren Kirby <bulliver@gmail.com>
2025-08-05 21:11:45 -0700

    Finish day 6, part 2

--- 2019/day6.cpp
+++ 2019/day6.cpp
@@ -11,6 +11,27 @@ int count_orbits(const unordered_map<string, vector<string>>& map, const string&
     return total;
 }
 
+bool find_path(set<string> seen, const string& node, int counter,
+    const unordered_map<string, vector<string>>& graph, const string& target) {
+    seen.insert(node);
+    if (node == target) {
+        cout << counter << endl;
+        return true;
+    }
+    auto it = graph.find(node);
+    if (it != graph.end()) {
+        for (const string& next_node : it->second) {
+            if (seen.contains(next_node)) {
+                continue;
+            }
+            if (find_path(seen, next_node, counter + 1, graph, target) == true) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 int main() {
     ifstream inputFile("../input/day6.txt");
 
@@ -20,7 +41,10 @@ int main() {
     }
 
     unordered_map<string, vector<string>> planet_map;
+    unordered_map<string, vector<string>> graph;
     string line;
+    string start;
+    string target;
 
     while (getline(inputFile, line)) {
         // Input format is: "AAA)BBB"
@@ -28,9 +52,22 @@ int main() {
             string parent = line.substr(0, 3);
             string child = line.substr(4, 3);
             planet_map[parent].push_back(child);
+            // for part 2
+            graph[child].push_back(parent);
+            graph[parent].push_back(child);
+            if (child == "YOU") {
+                start = parent;
+            }
+            if (child == "SAN") {
+                target = parent;
+            }
         }
     }
 
+    // Part 1
     cout << count_orbits(planet_map, "COM", 0) << endl;
+    // Part 2
+    set<string> seen;
+    find_path(seen, start, 0, graph, target);
     return 0;
 }

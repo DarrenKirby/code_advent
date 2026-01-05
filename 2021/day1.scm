24c9d1fbe1f90fcd03a6f5283e8b9f600dfe938a
Darren Kirby <bulliver@gmail.com>
2026-01-04 21:25:37 -0800

    New year using Cozenage Scheme!

---
+++ 2021/day1.scm
@@ -0,0 +1,24 @@
+;;; Solution written in Cozenage Scheme
+;;; https://github.com/DarrenKirby/cozenage
+
+; For cdddr
+(import (base cxr))
+
+(define (identity x) x)
+
+(define (count-increases lst)
+  (length (filter identity
+            (map < lst (cdr lst)))))
+
+(define thunk (lambda () (map string->number (read-lines))))
+
+(define input (with-input-from-file "input/day1.txt" thunk))
+
+;;; Part 1
+(println (count-increases input))
+
+;;; Part 2
+
+(println (length
+           (filter identity
+             (map < input (cdddr input)))))

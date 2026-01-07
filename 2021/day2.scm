bd57026fc05b2738e60fdda14e1d8462fe1bf3b6
Darren Kirby <bulliver@gmail.com>
2026-01-06 20:00:20 -0800

    Code cleanup

--- 2021/day2.scm
+++ 2021/day2.scm
@@ -6,18 +6,18 @@
     (with-input-from-file "input/day2.txt" thunk))
 
 (define (apply-move pos move)
-  (let ((x   (car pos))
-        (z   (cadr pos))
+    (let ((x (car pos))
+        (z (cadr pos))
         (cmd (string->symbol (car move)))
-        (n   (string->number (cadr move))))
+        (n (string->number (cadr move))))
     (case cmd
-      ((forward) (list (+ x n) z))
-      ((up)      (list x (- z n)))
-      ((down)    (list x (+ z n)))
-      (else      pos))))
+        ((forward) (list (+ x n) z))
+        ((up) (list x (- z n)))
+        ((down) (list x (+ z n)))
+        (else pos))))
 
 (define final-pos
-  (foldl apply-move '(0 0) input))
+    (foldl apply-move '(0 0) input))
 
 ;;; Part 1
 (println (* (car final-pos) (cadr final-pos)))

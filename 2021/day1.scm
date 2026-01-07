b93596c7c25524383a33965711ac315ae896e789
Darren Kirby <bulliver@gmail.com>
2026-01-06 16:15:44 -0800

    Fix formatting

--- 2021/day1.scm
+++ 2021/day1.scm
@@ -7,18 +7,20 @@
 (define (identity x) x)
 
 (define (count-increases lst)
-  (length (filter identity
-            (map < lst (cdr lst)))))
+    (length (filter identity
+        (map < lst (cdr lst)))))
 
-(define thunk (lambda () (map string->number (read-lines))))
+(define thunk
+    (lambda ()
+        (map string->number (read-lines))))
 
-(define input (with-input-from-file "input/day1.txt" thunk))
+(define input
+    (with-input-from-file "input/day1.txt" thunk))
 
 ;;; Part 1
 (println (count-increases input))
 
 ;;; Part 2
-
 (println (length
-           (filter identity
-             (map < input (cdddr input)))))
+    (filter identity
+        (map < input (cdddr input)))))
